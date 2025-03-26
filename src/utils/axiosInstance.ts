333a069ef27d3b66d1da36ab3ea09396b51c1e08
raghava20 <raghavaanand22@gmail.com>
2025-03-26 10:10:36 +0530

    fix:instance

--- src/utils/axiosInstance.ts
+++ src/utils/axiosInstance.ts
@@ -1,8 +1,21 @@
 import axios from "axios";
+import Cookies from "js-cookie";
 
 const API = axios.create({
   baseURL: "https://mypost-server.onrender.com/api",
   withCredentials: true, // Needed for HttpOnly cookies
 });
+
+// Attach Authorization token (if stored in cookies)
+API.interceptors.request.use(
+  (config) => {
+    const token = Cookies.get("token"); // Retrieve token from cookies
+    if (token) {
+      config.headers["Authorization"] = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
 
 export default API;
