45fb7fbcce1356aa569a3b583d4843b488c32620
raghava20 <raghavaanand22@gmail.com>
2025-03-26 08:54:17 +0530

    fix:backend url

--- src/utils/axiosInstance.ts
+++ src/utils/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://mypost-server.onrender.com/api",
   withCredentials: true, // Needed for HttpOnly cookies
 });
 
